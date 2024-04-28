ebb483a6ca107da7298d3ae32e92ca93b75b3330
syar23 <133503924+syar23@users.noreply.github.com>
2024-04-28 16:41:32 -0400

    Update linear.R

--- R/linear.R
+++ R/linear.R
@@ -7,15 +7,3 @@ main.linear <- function(X, y){
     fit <- lm(y~., data=df) # linear regression
   return(fit)
 }
-#Do we polynomial regression?
-
-
-# bagged.linear <- function(X, y){
-#   fit <- lm(y~., data=df)
-#   return(fit$coefficients)
-# }
-# 
-# bagged.linear <- function(df){
-#   fit <- glm(y ~ ., data = df, family = "binomial")
-#   return(fit$coefficients)
-# }
