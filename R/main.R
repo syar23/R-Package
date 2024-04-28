7e6b84f518e70d528e22b944ab885497ac75e635
syar23 <133503924+syar23@users.noreply.github.com>
2024-04-28 17:28:14 -0400

    Updated main.R

--- R/main.R
+++ R/main.R
@@ -1,78 +1,78 @@
-# function to get the model outputs
-my.models <- function(X, y, model.type, r.bagging, is.ensemble){
-  # Create an empty list
-  results <- list()
-  
-  for (mtype in model.type) {
-    if(r.bagging > 1){
-      source("bagging.R")
-      result <- bagged.model(X, y, r.bagging, model.type)
-    } else {
-      # load the appropriate source file
-      file.name <- paste0(mtype,".R")
-      source(file.name)
-      # call the appropriate model
-      if(mtype="linear")
-        result <- main.linear(X, y)
-      
-      if(mtype="ridge")
-        result <- main.ridge(X, y)
-      
-      if(mtype="lasso")
-        result <- main.lasso(X, y)
-      
-      if(mtype="elastic.net")
-        result <- main.elastic.net(X, y)
-      
-      if(mtype="random.forest")
-        result <- main.random.forest(X, y)
-    }
-    
-    results[[mtype]] <- result
-  }
-  
-  if(is.ensemble){
-    source("ensemble.learning.R")
-    results[["ensembled"]] <- main.ensemble.learning(X, y)
-  }
-  
-  return(results)
-}
+#' Simple Ensemble Learning
+#'
+#' This function performs ensemble learning using a combination of linear models (such as Elastic Net) and tree-based models (such as Random Forest).
+#' It conducts preliminary data screening, trains multiple models, and optionally performs ensemble learning.
+#'
+#' @param X Matrix of predictors (independent variables).
+#' @param y Response variable (dependent variable).
+#' @param models A character vector specifying the types of models to include in the ensemble. Options include "elastic.net" and "random.forest".
+#' @param k Number of predictors to select in the pre-screening step. Default is half the number of predictors.
+#' @param r.bagging Number of times to perform bagging for each model. Default is 50.
+#' @param is.ensemble Logical indicating whether to perform ensemble learning. Default is FALSE.
+#'
+#' @return A list containing the results of the ensemble learning process.
+#'
+#' The returned list includes the results of training and evaluating individual models specified in 'models', as well as the ensemble model if 'is.ensemble' is TRUE.
+#'
+#' @export
+#'
+#' @examples
+#' # Example usage:
+#' results <- simpleEnsembleGroup22(X, y, models = c("elastic net", "random.forest"), r.bagging = NULL, is.ensemble = TRUE)
+simpleEnsembleGroup22 <- function(X, y, models = c("elastic net", "random forest"), r.bagging = NULL, is.ensemble = FALSE, k=NULL) {
 
-# function to check if the provided predictors are good or not.
-is.good.predictor <- function(x){
-  if(is.numeric(x)){
-    return(TRUE)
-  } else{
-    # Check if all factor column has only two levels
-    if(is.factor(x) && length(unique(x))==2)
-      return(TRUE)
-    else
-      return(FALSE)
-  }
-}
-
-
-#model.type = c("linear", "ridge", "lasso", "elastic.net", "random.forest") 
-simpleEnsembleGroup22 <- function(X, y, model.type = c("elastic.net", "random.forest"), k = ncol(X)/2, r.bagging = 50, is.ensemble = FALSE){
   #Pre-screening to check if provided data is valid
   if(is.null(X) || is.null(y)){
     return("Either X and/or y is NULL")
   }
-  # y either binary or continuous -- if cont. it is assumed to be normally dist. 
-  
+  # y either binary or continuous -- if cont. it is assumed to be normally dist.
+
   if (!is.numeric(y) || (is.factor(y) && length(unique(y))>2) )
-    return("The response varibale must be numeric: either binary or continuous!")
-  
+    return("The response variable must be numeric: either binary or continuous!")
+
   # Identify if all column's data types are acceptable: X can be continuous, discrete, binary
-  is.good.data <- sapply(X, is.good.predictor)
-  if(sum(is.good.data) < length(is.good.data))
+  if (!all(sapply(X, function(x) is.numeric(x) || (is.factor(x) && length(unique(x)) == 2)))) {
     return("The predictor variable must be numeric: binary, discrete, or continuous.")
-  
+  }
+
   #variable pre-screening
+  print("variable screening is being done - ")
   source("variable screening.R")
-  X <- variable_pre_screening(X, y, k)
-  return(my.models(X, y, model.type, r.bagging, is.ensemble))
+  X <- variable_screening(X, y, k)
+
+  results <- list()
+
+  for (modeltype in models) {
+    if (!is.null(r.bagging)) {
+      print("Bagging is being done - ")
+      source("bagging.R")
+      result <- bagged.model(X, y, r.bagging, models)
+    } else {
+      # load the appropriate source file
+      print("List of models given - ")
+      print(modeltype)
+      file.name <- paste0(modeltype,".R")
+      source(file.name)
+
+      # call the appropriate model using switch
+      result <- switch(modeltype,
+                       linear = linear_model(X, y),
+                       ridge = ridge_model(X, y),
+                       lasso = lasso_model(X, y),
+                       elastic.net = elastic_net_regression(X, y),
+                       random.forest = rf_model(X, y))
+    }
+
+    results[[modeltype]] <- result
+  }
+
+  if (is.ensemble) {
+    print("Ensemble is being done - ")
+    source("ensemble.R")
+    results[["ensembled"]] <- ensemble_predict(X, y)
+  }
+
+  return(results)
 }
 
 
