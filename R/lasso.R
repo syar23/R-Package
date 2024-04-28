91f40cede03a4d7c0321a3d5c9bce8e51bec79bc
Aman Arya <53216120+amnry@users.noreply.github.com>
2024-04-28 16:44:15 -0400

    Update lasso.R

--- R/lasso.R
+++ R/lasso.R
@@ -6,9 +6,9 @@
 #' @param X Matrix of predictor variables. Each row represents an observation, and each column represents a predictor.
 #' @param y Response variable. For binary outcomes, it should be a binary vector (0 or 1). For continuous outcomes, it should be a numeric vector.
 #' @param fold_percentage Ratio of number of folds to sample size for cross-validation.
-#' 
+#'
 #' @return A fitted Lasso regression model object of class "glmnet".
-#' 
+#'
 #' @examples
 #' # Example usage
 #' X <- matrix(rnorm(100 * 5), ncol = 5)
@@ -18,26 +18,30 @@
 #' @import glmnet
 #' @export
 lasso_model <- function(X, y, fold_percentage = 0.2) {
-  # Determine the type of response variable
-  if (is.factor(y) || all(y %in% c(0, 1))) {
-    family_type <- "binomial"
-  } else {
-    family_type <- "gaussian"
+  # Ensure X is a matrix
+  if (!is.matrix(X)) {
+    X <- as.matrix(X)
   }
-  # Calculate the number of folds based on the fold percentage
-  #nfolds <- max(2, round(1 / fold_percentage))
-  # Calculate the number of folds ensuring each fold has at least 10 observations
+
+  # Determine the type of response variable
+  family_type <- if (is.factor(y) || all(y %in% c(0, 1))) "binomial" else "gaussian"
+
+  # Calculate the number of folds ensuring each fold has at least a minimum number of observations
   min_obs_per_fold <- 10
-  nfolds <- max(2, min(floor(nrow(X) / min_obs_per_fold), round(1 / 0.2)))
-  
+  nfolds <- max(2, min(floor(nrow(X) / min_obs_per_fold), round(1 / fold_percentage)))
+
   # Perform cross-validation to select the optimal lambda value
   cv_model <- glmnet::cv.glmnet(X, y, family = family_type, nfolds = nfolds)
-  
-  # Select the lambda value with minimum cross-validated error
-  best_lambda <- cv_model$lambda.min
-  
+
   # Fit the final Lasso model with the selected lambda
+  best_lambda <- cv_model$lambda.min
   model <- glmnet::glmnet(X, y, family = family_type, lambda = best_lambda)
-  
+
   return(model)
 }
+
+
+# data(cpus)
+# str(cpus)
+# resluts <- lasso_model(cpus[, 2:7], cpus[,8])
+# print(results)
