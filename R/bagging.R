d5a6caba79d8270f232528e760e82119f0e29fcc
syar23 <133503924+syar23@users.noreply.github.com>
2024-04-28 21:00:15 -0400

    Update bagging.R

--- R/bagging.R
+++ R/bagging.R
@@ -1,76 +1,71 @@
-bagged.model <- function(X, y, r.bagging, mtype){
-  #frame a dataset
-  df.original = data.frame(y,X)
-  y.hats <- matrix(1, nrow = nrow(df.original), ncol = r.bagging)
-  naive.mat <- matrix(1, ncol = ncol(df.original) + 1, nrow = r.bagging)
-  for (i in 1:r.bagging) {
-    # sampling from the original data set
-    df_temp <- df.original[sample(1:nrow(df.original), replace = TRUE),]
-    
-    # load the appropriate source file
-    file.name <- paste0(mtype,".R")
-    source(file.name)
-    # call the appropriate model
-    if(mtype=="linear"){
-      fit <- main.linear(X, y)
-      X <- data.frame(X)
-    } else if(mtype=="ridge"){
-      fit <- ridge_model(X, y)
-      X <- as.matrix(X)
-    } else if(mtype=="lasso"){
-      fit <- lasso_model(X, y)
-      X <- as.matrix(X)
-      fitCoefs <- coef(fit)
-      terms <- which(fitCoefs != 0)
-    } else if(mtype=="elastic net"){
-      fit <- elastic_net_regression(X, y)
-      fitCoefs <- coef(fit)
-      terms <- which(fitCoefs != 0)
-    } else if(mtype=="random forest"){
-      fit <- rf_model(X, y)
-      fitCoefs <- coef(fit)
-      terms <- which(fitCoefs != 0)
-    }
+#' Load Model Function
+#'
+#' This function loads the appropriate model based on the specified type.
+#'
+#' @param X Independent variables (predictors).
+#' @param y Dependent variable (response).
+#' @param mtype Type of machine learning model (linear, ridge, lasso, elastic net, random forest).
+#' @return Fitted model.
+load_model <- function(X, y, mtype) {
+  file.name <- paste0(mtype, ".R")
+  source(file.name)
+  switch(mtype,
+         linear = main.linear(X, y),
+         ridge = ridge_model(as.matrix(X), y),
+         lasso = lasso_model(as.matrix(X), y),
+         "elastic net" = elastic_net_regression(X, y),
+         "random forest" = rf_model(X, y)
+  )
+}
 
-    y.hats[,i] <- predict(fit,X)
-    if(mtype=="lasso" || mtype=="elastic net" || mtype=="random forest"){
-      for (j in terms)
-        naive.mat[j,i] <- 1
-    }
-  }
-  
-  majority <- function(x){
-    max(as.numeric(names(which.max(table(x)))))
-  }
-  
-  if(mtype=="lasso" || mtype=="elastic net" || mtype=="random forest")
-    apply(naive.mat, 2, sum)
-  
-  if(is.factor(y)){
-    # majority vote for binary response variable & naive var for three models
-    if(mtype=="lasso" || mtype=="elastic net" || mtype=="random forest")
-      return(list(fitted.values = apply(y.hats, 1, majority), naive = apply(naive.mat, 2, sum)))
-    else
-      return(fitted.values = apply(y.hats, 1, majority)) # majority vote for binary response variable
-  } else{
-    # simple mean & naive var for three models
-    if(mtype=="lasso" || mtype=="elastic net" || mtype=="random forest")
-      return(list(fitted.values = apply(y.hats, 1, mean), naive = apply(naive.mat, 2, sum)))
-    else
-      return(fitted.values = apply(y.hats, 1, mean)) # simple mean
+#' Process Model Results Function
+#'
+#' This function processes the results of the fitted model based on the specified type.
+#'
+#' @param fitted Fitted values.
+#' @param mtype Type of machine learning model (linear, ridge, lasso, elastic net, random forest).
+#' @param terms Terms.
+#' @param y.hats Predicted values.
+#' @return Processed results.
+process_results <- function(fitted, mtype, terms, y.hats) {
+  if (mtype %in% c("lasso", "elastic net", "random forest")) {
+    naive.mat <- matrix(1, ncol = length(fitted) + 1, nrow = nrow(y.hats))
+    for (j in terms)
+      naive.mat[j, ] <- 1
+    list(fitted.values = fitted, naive = apply(naive.mat, 2, sum))
+  } else {
+    fitted
   }
 }
 
-
-#implementation
-# df <- data.matrix(cats)
-# colnames(df) <- c("y", sprintf("x%d", 1:(ncol(df)-1)))
-# X <- df[,2:3]
-# y <- df[,1]
-# y[y==2] <- 0
-# 
-# X <- cement[,1:4]
-# y <- cement[,5]
-# y_pred <- bagged.model(X,y,10,"elastic net")
-# y_pred
-#"C:/Users/hmoon/Desktop/AMS/AMS 597/Group Project/",
+#' Bagged Model Function
+#'
+#' This function performs bagging with different machine learning models.
+#'
+#' @param X Independent variables (predictors).
+#' @param y Dependent variable (response).
+#' @param r.bagging Number of bagging iterations.
+#' @param mtype Type of machine learning model (linear, ridge, lasso, elastic net, random forest).
+#' @return If the response variable is factor, returns a list with fitted values and naive variables; 
+#' otherwise, returns fitted values.
+#' @export
+bagged_model <- function(X, y, r.bagging, mtype) {
+  # Frame a dataset
+  df.original <- data.frame(y, X)
+  y.hats <- matrix(1, nrow = nrow(df.original), ncol = r.bagging)
+  for (i in 1:r.bagging) {
+    # Sampling from the original data set
+    df_temp <- df.original[sample(1:nrow(df.original), replace = TRUE),]
+    # Load and fit the model
+    fit <- load_model(df_temp[, -1], df_temp[, 1], mtype)
+    # Predict
+    y.hats[, i] <- predict(fit, df_temp[, -1])
+  }
+  
+  # Process results
+  if (is.factor(y)) {
+    process_results(apply(y.hats, 1, function(x) max(as.numeric(names(which.max(table(x)))))), mtype, NULL, y.hats)
+  } else {
+    process_results(apply(y.hats, 1, mean), mtype, NULL, y.hats)
+  }
+}
