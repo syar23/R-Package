6b0029e0dce51b79bda702326dababc88c3e4bc7
Hmmoonam <48888381+Hmmoonam@users.noreply.github.com>
2024-04-28 23:53:32 -0400

    all good except the elastic net. It may need some parameters with the predict method.

--- R/bagging.R
+++ R/bagging.R
@@ -10,32 +10,12 @@ load_model <- function(X, y, mtype) {
   file.name <- paste0(mtype, ".R")
   source(file.name)
   switch(mtype,
-         linear = main.linear(X, y),
+         linear = linear_model(X, y),
          ridge = ridge_model(as.matrix(X), y),
          lasso = lasso_model(as.matrix(X), y),
          "elastic_net" = elastic_net_regression(X, y),
          "random_forest" = rf_model(X, y)
   )
-}
-
-#' Process Model Results Function
-#'
-#' This function processes the results of the fitted model based on the specified type.
-#'
-#' @param fitted Fitted values.
-#' @param mtype Type of machine learning model (linear, ridge, lasso, elastic net, random forest).
-#' @param terms Terms.
-#' @param y.hats Predicted values.
-#' @return Processed results.
-process_results <- function(fitted, mtype, terms, y.hats) {
-  if (mtype %in% c("lasso", "elastic_net", "random_forest")) {
-    naive.mat <- matrix(1, ncol = length(fitted) + 1, nrow = nrow(y.hats))
-    for (j in terms)
-      naive.mat[j, ] <- 1
-    list(fitted.values = fitted, naive = apply(naive.mat, 2, sum))
-  } else {
-    fitted
-  }
 }
 
 #' Bagged Model Function
@@ -53,19 +33,44 @@ bagged_model <- function(X, y, r.bagging, mtype) {
   # Frame a dataset
   df.original <- data.frame(y, X)
   y.hats <- matrix(1, nrow = nrow(df.original), ncol = r.bagging)
+  naive.mat <- matrix(1, nrow = r.bagging, ncol = ncol(df.original))
   for (i in 1:r.bagging) {
     # Sampling from the original data set
     df_temp <- df.original[sample(1:nrow(df.original), replace = TRUE),]
     # Load and fit the model
     fit <- load_model(df_temp[, -1], df_temp[, 1], mtype)
+    if (mtype == "ridge")
+      fitCoefs <- coef(fit, alpha = 0)
+    else if (mtype == "lasso")
+      fitCoefs <- coef(fit, alpha = 1)
+    else
+      fitCoefs <- coef(fit)
+    terms <- which(fitCoefs != 0)
+    terms <- terms[terms != 1]
+    terms <- terms - 1
     # Predict
-    y.hats[, i] <- predict(fit, df_temp[, -1])
+    if(mtype == "linear")
+      X_temp <- df_temp[, -1]
+    else
+      X_temp <- as.matrix(df_temp[, -1])
+    y.hats[, i] <- predict(fit, X_temp)
+    
+    if (mtype %in% c("lasso", "elastic_net", "random_forest")) {
+      for (j in terms)
+        naive.mat[i,j] <- 1
+    }
   }
-
+  
   # Process results
   if (is.factor(y)) {
-    process_results(apply(y.hats, 1, function(x) max(as.numeric(names(which.max(table(x)))))), mtype, NULL, y.hats)
+    y.hat = apply(y.hats, 1, function(x) max(as.numeric(names(which.max(table(x))))))
   } else {
-    process_results(apply(y.hats, 1, mean), mtype, NULL, y.hats)
+    y.hat = apply(y.hats, 1, mean)
+  }
+  
+  if (mtype %in% c("lasso", "elastic_net", "random_forest")){
+    return(list(fitted.values = y.hat, naive = apply(naive.mat, 2, sum)))
+  } else {
+    return(fitted.values = y.hat)
   }
 }
