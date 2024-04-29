f51295c0d215ebe9a0038dbb000efb70d9859c5d
syar23 <133503924+syar23@users.noreply.github.com>
2024-04-28 21:08:13 -0400

    Update linear.R

--- R/linear.R
+++ R/linear.R
@@ -1,9 +1,23 @@
-#linear Model
-main.linear <- function(X, y){
-  df = data.frame(y,X)
-  if(is.factor(y))
-    fit <- glm(y ~ ., data = df, family = "binomial") # logistic regression
-  else
-    fit <- lm(y~., data=df) # linear regression
+#' Linear Model Function
+#'
+#' This function fits a linear model using either linear regression for continuous response variables
+#' or logistic regression for binary response variables.
+#'
+#' @param X Independent variables (predictors).
+#' @param y Dependent variable (response).
+#' @return Fitted linear model object.
+#' @examples
+#'
+#' # Example usage:
+#' # linear_model(X, y)
+#'
+#' @export
+linear_model <- function(X, y) {
+  df <- data.frame(y, X)
+  if (is.factor(y)) {
+    fit <- glm(y ~ ., data = df, family = "binomial")  # logistic regression
+  } else {
+    fit <- lm(y ~ ., data = df)  # linear regression
+  }
   return(fit)
 }
